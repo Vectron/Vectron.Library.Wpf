370080a831bc257f62969e9d5ad303e7a1f3557d
Vectron <thijs_bloebaum@hotmail.com>
2024-07-12 15:55:06 +0200

    add XmlnsPrefix and XmlnsDefinition

--- src/Vectron.Library.Wpf/AssemblyInfo.cs
+++ src/Vectron.Library.Wpf/AssemblyInfo.cs
@@ -1,3 +1,10 @@
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsPrefix("http://schemas.vectron.me/xaml", "vec")]
+[assembly: XmlnsDefinition("http://schemas.vectron.me/xaml", "Vectron.Library.Wpf")]
+[assembly: XmlnsDefinition("http://schemas.vectron.me/xaml", "Vectron.Library.Wpf.Behaviors")]
+[assembly: XmlnsDefinition("http://schemas.vectron.me/xaml", "Vectron.Library.Wpf.Controls")]
+[assembly: XmlnsDefinition("http://schemas.vectron.me/xaml", "Vectron.Library.Wpf.Converters")]
+[assembly: XmlnsDefinition("http://schemas.vectron.me/xaml", "Vectron.Library.Wpf.Dialogs")]
